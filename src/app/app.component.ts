6d4be6ffbedae03e0a9303504ca1cbd02f78b380
Sara <seking13@gmail.com>
2017-07-18 17:37:13 -0600

    secondApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,27 +18,7 @@ zoom: number = 10;
   markerDraggable: string;
   //markers
   markers: marker[] = [
-    {
-      name: 'Museum of Nature and Science',
-      lat: 39.7475,
-      lng: -104.9428,
-      draggable: true,
-      openInfoWindow: true
-    },
-    {
-      name: 'Corrs Field',
-      lat: 39.7559,
-      lng: -104.9942,
-      draggable: true,
-      openInfoWindow: true
-    },
-    {
-      name: 'Union Station',
-      lat: 39.7537,
-      lng: -105.0007,
-      draggable: true,
-      openInfoWindow: true
-    }
+  
   ];
 
   constructor(){
---
+++ src/app/init-markers.ts
@@ -0,0 +1,35 @@
+export class Init {
+  load(){
+    if(localStorage.getItem('markers') === null || localStorage.getItem('markers') === undefined){
+      console.log('no markers found...creating')
+
+      var markers = [
+        {
+          name: 'Museum of Nature and Science',
+          lat: 39.7475,
+          lng: -104.9428,
+          draggable: true,
+          openInfoWindow: true
+        },
+        {
+          name: 'Corrs Field',
+          lat: 39.7559,
+          lng: -104.9942,
+          draggable: true,
+          openInfoWindow: true
+        },
+        {
+          name: 'Union Station',
+          lat: 39.7537,
+          lng: -105.0007,
+          draggable: true,
+          openInfoWindow: true
+        }
+      ];
+
+      localStorage.setItem('markers', JSON.stringify('markers') )
+    } else {
+      console.log('Loading markers....')
+    }
+  }
+}
