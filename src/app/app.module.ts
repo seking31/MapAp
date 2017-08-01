c6debf79c58cf3818d8cc3bf3b720a701453435b
Sara <seking13@gmail.com>
2017-08-01 16:19:23 -0600

    jumbo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,27 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AgmCoreModule } from '@agm/core';
+import { RouterModule, Routes }   from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    ),
     BrowserModule,
     CommonModule,
     FormsModule,
@@ -19,7 +32,7 @@ import { AppComponent } from './app.component';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyC02217vCI0-xrBs0jMi42buvscAn9M6Mo'
     })
-  ],
+ ],
   providers: [],
   bootstrap: [AppComponent]
 })
