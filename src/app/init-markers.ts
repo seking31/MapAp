4ef4c5c807e92073ed15542c23d334bc5a579d92
Sara <seking13@gmail.com>
2017-08-01 18:31:48 -0600

    Good

--- src/app/init-markers.ts
+++ src/app/init-markers.ts
@@ -24,6 +24,27 @@ export class Init {
           lng: -105.0007,
           draggable: true,
           openInfoWindow: true
+        },
+        {
+          name: 'Museum of Nature and Science',
+          lat: 39.7475,
+          lng: -104.9428,
+          draggable: true,
+          openInfoWindow: true
+        },
+        {
+          name: 'Corrs Field',
+          lat: 39.7559,
+          lng: -104.9942,
+          draggable: true,
+          openInfoWindow: true
+        },
+        {
+          name: 'Union Station',
+          lat: 39.7537,
+          lng: -105.0007,
+          draggable: true,
+          openInfoWindow: true
         }
       ];
 
